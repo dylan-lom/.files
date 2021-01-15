8e3ceb0086a32d5fad5f6523592bb59c7e702c12
Dylan Lom <dylanlom@gmail.com>
2021-01-16 00:18:04 +1100

    Add NOAUTO flag to install script
    If a directory contains a file called NOAUTO, it's install.sh won't be
    called unless the root install.sh is run with the -u flag

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
 for f in *; do
     test ! -d "$f" && continue
+    test -f "$f/NOAUTO" -a "$1 != -u" && \
+        echo "SKIPPING $f DUE TO NOAUTO" && \
+        echo "Run with -u flag to install" &&\
+        echo "----------------" &&\
+        continue
+
     echo "INSTALLING $f"
     echo "----------------"
     "./$f/install.sh"
     echo "----------------"
 done
+
